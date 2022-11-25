fcc590ddcbe825b7a4566b61a6f6f62b2af1665a
Alexander <trainee.alex.skowronski@sigmalabs.co.uk>
2022-11-25 14:36:29 +0000

    First attempt, local server works, need to make page look nice, edit property of exercises table, populate it, add foreign keys

---
+++ api.py
@@ -0,0 +1,29 @@
+from flask import Flask, current_app, jsonify
+import psycopg2
+import psycopg2.extras
+
+app = Flask(__name__)
+
+def database_connection(database_name, user_name, host_name):
+    conn = psycopg2.connect(f"dbname={database_name} user={user_name} host={host_name}")
+    return conn
+
+#really should change name of database to personal_project
+conn = database_connection('wlodarczyk', 'wlodarczyk', 'localhost')
+
+def execute_sql_query(query):
+    if conn != None:
+        with conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor) as curs:
+            curs.execute(query)
+            returned_data = curs.fetchall()
+            return returned_data
+
+@app.route("/", methods=["GET"])
+def index():
+    return current_app.send_static_file("index.html")
+
+@app.route("/exercises", methods=["GET"])
+def exercises():
+    query = "SELECT * FROM exercises;"
+    response = execute_sql_query(query)
+    return jsonify(exercises = response)
---
+++ static/index.js
@@ -0,0 +1,27 @@
+window.onload = async function load() {
+  getExercises();
+};
+
+async function getExercises() {
+  host = "http://127.0.0.1:5000/stories";
+  const result = await fetch(host, {
+    method: "GET",
+    credentials: "include",
+  });
+  const data = await result.json();
+  displayExercises(data.exercises);
+}
+
+function displayExercises(exercises) {
+  exercises.forEach(createExercise);
+}
+
+function createExercise(exercise) {
+  const exercises = document.getElementById("exercises");
+  const exerciseWrapper = document.createElement("div");
+  exerciseWrapper.innerHTML = `
+    <p>
+        <a class="name" href=${exercise.url}>${exercise.name} </a>
+    </p>`;
+  exercises.append(exerciseWrapper);
+}
